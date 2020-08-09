c691fc22f1e8d856c8d6fad615581a9bc0f14b6a
ntdkhiem <ppkhiemnguyen@gmail.com>
2020-08-08 23:54:27 -0400

    :green_heart: render html files from templates/

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request, jsonify
+
+from utils import getState
 
 app = Flask(__name__)
 
@@ -10,18 +13,40 @@ def index():
 
 @app.route('/diagnosis')
 def diagnosis():
-    return 'Hello from diagnosis!' 
+    return render_template('diagnosis.html') 
 
 
 @app.route('/tracker')
 def tracker():
-    return 'Hello from Tracker!' 
+    return render_template('tracker.html') 
 
 
 @app.route('/faq')
 def faq():
-    return 'Hello from FAQ!' 
+    return render_template('faq.html') 
+
+
+@app.route('/calculation')
+def calculation():
+    return render_template('calculation.html') 
+
+
+@app.route('/api/covid', methods=['GET'])
+def covid():
+    if request.args.get('lat') and request.args.get('lng'):
+        lat = request.args.get('lat')
+        lng = request.args.get('lng')
+
+        state = getState(lat, lng).lower()
+
+        data = requests.get('https://covid19-us-api.herokuapp.com/county').json()
+        # filter counties by state
+        entries = [d for d in data['message'] if d['state_name'].lower() == state]
+        if entries:
+            return jsonify(entries), 200
+    
+    return {'status': 'Unable to retrieve data.'}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='80', debug=True)
